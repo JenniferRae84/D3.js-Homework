0edfc9fea4f0d29a234c0e9b927a7e73beae33c2
Jenny <JenniferRae84@gmail.com>
2019-03-30 15:25:03 -0600

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,114 +6,113 @@ var width = 800;
 var margin = {
   top: 40,
   right: 40,
-  bottom: 40,
-  left: 40
+  bottom: 60,
+  left: 60
 };
 
-// // chart area minus margins
-// var chartHeight = height - margin.top - margin.bottom;
-// var chartWidth = width - margin.left - margin.right;
+// chart area minus margins
+var chartHeight = height - margin.top - margin.bottom;
+var chartWidth = width - margin.left - margin.right;
 
 // create svg container
 var svg = d3.select("#scatter").append("svg")
     .attr("height", height)
     .attr("width", width)
-    .style("background-color","abcdef");
+    //.style("background-color","abcdef");  // used this to see where the svg was
 
 // shift everything over by the margins
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+//// Import data ////     
 // http://learnjsdata.com/read_data.html
-d3.csv("data.csv").then(function(data) {
-        console.log(data[0]);
-      });
-      
+d3.csv("assets/js/data.csv").then(function(healthData) {
+    //console.log(healthData[0]);
+    healthData.forEach(function (d) {
+        // d.age = +d.age;
+        // d.ageMoe = +d.ageMoe;
+        d.healthcare = +d.healthcare;
+        // d.healthcareHigh = +d.healthcareHigh;
+        // d.healthcareLow = +d.healthcareLow;
+        // d.income = +d.income;
+        // d.incomeMoe = +d.incomeMoe;
+        // d.obesity = +d.obesity;
+        // d.obesityHigh = +d.obesityHigh;
+        // d.obesityLow = +d.obesityLow;
+        d.poverty = +d.poverty;
+        // d.povertyMoe = +d.povertyMoe;
+        // d.smokes = +d.smokes;
+        // d.smokesHigh = d.smokesHigh;
+        // d.smokesLow = +d.smokesLow;
+        // state_abbr = d.abbr
+        // console.log(state_abbr);
 
-////Jeff's Code   
-// function renderData(xData, yData) {
-//   d3.csv('data.csv').then((data) => {
-//       d3.selectAll('circle').remove()
-//       chartGroup.selectAll("g").remove()
-//       chartGroup.selectAll("text").remove()
-      
-//       let colNames = Object.keys(d3.values(data)[0]);
-//       console.log(colNames)
-//       data.forEach(x => {
-//           x.xData = +x[xData];
-//           x.yData = +x[yData]
-//       })
-//       // compare income and yData
-//       xScale = d3.scaleLinear()
-//           .domain(d3.extent(data, d => d.xData))
-//           .range([0, width])
-//       bottomAxis = d3.axisBottom(xScale)
-//       yScale = d3.scaleLinear()
-//           .domain(d3.extent(data, d => d.yData))
-//           .range([height, 0]) // value -> display
-//       leftAxis = d3.axisLeft(yScale)
-//       chartGroup.append("g")
-//           .attr("transform", `translate(0, ${height})`)
-//           .call(bottomAxis);
-//       chartGroup.append("g")
-//           .call(leftAxis);
-//     });
-// }
+    })
+        
+        //// Creating scale functions ////
+        // scale y to chart height
+        var xScale = d3.scaleLinear()
+            .domain([d3.min(healthData, d => d.poverty), d3.max(healthData, d => d.poverty)])
+            .range([0, chartWidth]);
+
+        var yScale = d3.scaleLinear()
+            .domain([d3.min(healthData, d => d.healthcare), d3.max(healthData, d => d.healthcare)])
+            .range([chartHeight, 0]);
 
 
+        //// Set up axes ////
+        // create axes
+        var yAxis = d3.axisLeft(yScale);
+        var xAxis = d3.axisBottom(xScale);
 
+        // set x to the bottom of the chart
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(xAxis);
 
+        // set y to the y axis
+        chartGroup.append("g")
+            .call(yAxis);
+        
+        // create axes labels
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText")
+            .text("Lacks Healthcare (%)");
 
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 3}, ${margin.bottom + chartHeight - 10})`)
+            .attr("class", "axisText")
+            .text("In Poverty (%)");
+          
+        //// Circles ////
+        // Create Circles
+        var circlesGroup = chartGroup.selectAll("circle")
+            .data(healthData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xScale(d.poverty))
+            .attr("cy", d => yScale(d.healthcare))
+            .attr("r", "10")
+            .attr("fill", "blue")
+            .attr("opacity", ".5");
 
-
-
-
-
-
-
-
-
-
-
-
-// // Step 2: Create scale functions
-// // scale y to chart height
-// var yScale = d3.scaleLinear()
-//     .domain([0, d3.max(d.healthcare)]) 
-//     .range([chartHeight, 0]);
-
-// // scale x to chart width
-// var xScale = d3.scaleBand()
-//     .domain(d.poverty)
-//     .range([0, chartWidth])
-//     .padding(0.1);
-
-// // create axes
-// var yAxis = d3.axisLeft(yScale);
-// var xAxis = d3.axisBottom(xScale);
-
-// // set x to the bottom of the chart
-// chartGroup.append("g")
-//     .attr("transform", `translate(0, ${chartHeight})`)
-//     .call(xAxis);
-
-// // set y to the y axis
-// chartGroup.append("g")
-//     .call(yAxis);
-
-
-
-// //set x & y axis to the data 
-
-
-
-
-//PENELTY BOX:
-// // import data from the csv
-// d3.csv('data.csv').then(function(healthData) {
-//         healthData.forEach(function(d) {
-//             d.healthcare = +d.healthcare;
-//             d.poverty = +d.poverty;
-//         });
-//     console.log(d.healthcare)
-// });
+        // Add text to circles
+        chartGroup.selectAll('circle.text')
+            .data(healthData)
+            .enter()
+            .append("text")
+            .attr("x", d => xScale(d.poverty))
+            .attr("y", d => yScale(d.healthcare))
+            .attr("dy", 5)
+            .classed("stateText", true)
+            .attr("font-size", "10px")
+            .text(function (d) {
+                return d.abbr;
+        });
+    });
+   
+    
